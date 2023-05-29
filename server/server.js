2182948977c3afd30ac53b1acdf727b5161e69d1
Steven <stevenlin3263@gmail.com>
2023-05-29 20:47:45 +0800

    Connect to the MongoDB

--- server/server.js
+++ server/server.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql').graphqlHTTP;
+const mongoose = require('mongoose');
 const schema = require('./schema/schema');
 
 const app = express();
+
+require('dotenv').config();
+
+mongoose.set('strictQuery', false);
+console.log(process.env.MONGO_URL);
+mongoose.connect(process.env.MONGO_URL).then(() => {
+  console.log('DB Connection is successful')
+}).catch((error) => {
+  console.log(error.message)
+})
 
 app.use('/graphql', graphqlHTTP({
   schema,
