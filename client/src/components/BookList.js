2f8b25ef2194351e9a28119e7c7015ce64bcc0cd
Steven <stevenlin3263@gmail.com>
2023-06-01 09:14:23 +0800

    Rendering Data in a Component

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -6,11 +6,21 @@ import { useQuery } from '@apollo/react-hooks';
 const BookList = () => {
   const { loading, error, data } = useQuery(GET_BOOKS_QUERY);
 
+  if (loading) return <p>Loading ...</p>
+  if (error) return <p>Error ...</p>
+
+  const { books } = data;
+  const bookListItems = books.map(({ id, name }) => {
+    return (
+      <li key={id}>{name}</li>
+    )
+  })
+
   console.log('data', data);
   return (
     <div>
       <ul id="book-list">
-        <li>Book Name</li>
+        {bookListItems}
       </ul>
     </div>
   )
